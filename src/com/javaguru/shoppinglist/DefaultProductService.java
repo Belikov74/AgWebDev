242b5788bf16bd7f44cc6cc0e3c0106a5f14196e
Denis <40504407+Belikov74@users.noreply.github.com>
2019-03-17 20:21:39 +0200

    Add files via upload

---
+++ src/com/javaguru/shoppinglist/DefaultProductService.java
@@ -0,0 +1,32 @@
+package com.javaguru.shoppinglist;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class DefaultProductService implements ProductService {
+
+    private Map<Long, Product> database = new HashMap<>();
+    private Long PRODUCT_ID_SEQUENCE = 0L;
+
+    public Product findBy(Long id) {
+        if (id == null) {
+            throw new IllegalArgumentException("Id must be not null");
+        }
+        return database.get(id);
+    }
+    @Override
+    public Long create(Product product) {
+        if (product == null) {
+            throw new IllegalArgumentException("Cannot be null");
+        }
+        product.setId(PRODUCT_ID_SEQUENCE);
+
+        database.put(PRODUCT_ID_SEQUENCE, product);
+        return PRODUCT_ID_SEQUENCE++;
+    }
+
+ //   цена продукта должна быть больше 0
+ //   скидка не может быть больше 100 процентов
+ //   название не может быть меньше 3 символов и больше 32
+
+}
